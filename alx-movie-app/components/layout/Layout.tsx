4670f7b3dfb03b83fa31f6d6763634d9560defcb
Amiko Elvis <amikoelvis@gmail.com>
2025-08-06 23:04:30 +0300

    Set up Header, Footer, Layout and landing page

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,14 +1,15 @@
+import { ComponentProps } from "@/interfaces";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
-  );
-};
+    </>
+  )
+}
 
 export default Layout;
